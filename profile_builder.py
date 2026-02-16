05ad5e31d8877392f099b1a658e7f78edb66358b
Akash Dhanvate <dhanvateakash01@gmail.com>
2026-02-16 17:42:03 +0530

    Add files via upload

---
+++ profile_builder.py
@@ -0,0 +1,47 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "22fc884b-26a2-4889-8fab-a9575fc8140c",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "first_name = input(\"Enter first name: \")\n",
+    "last_name = input(\"Enter last name: \")\n",
+    "age = input(\"Enter age: \")\n",
+    "city = input(\"Enter city: \")\n",
+    "hobby = input(\"Enter hobby: \")\n",
+    "full_name = first_name + \" \" + last_name\n",
+    "print(\"\\n========== STUDENT PROFILE ==========\")\n",
+    "print(\"Full Name:\", full_name)\n",
+    "print(\"Age:\", age)\n",
+    "print(\"City:\", city)\n",
+    "print(\"Favorite Hobby:\", hobby)\n",
+    "print(\"Welcome to Python, \" + full_name + \"!\")\n",
+    "print(\"=====================================\")\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.14.2"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
